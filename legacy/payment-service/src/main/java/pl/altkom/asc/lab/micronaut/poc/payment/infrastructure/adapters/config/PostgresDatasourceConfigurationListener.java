7a06f540b501074a47f167435ecda3454e954555
Igor Baiborodine <igor.baiborodine@yandex.com>
2025-12-12 17:38:15 -0500

    feat: migrate payment service to postgres

---
+++ legacy/payment-service/src/main/java/pl/altkom/asc/lab/micronaut/poc/payment/infrastructure/adapters/config/PostgresDatasourceConfigurationListener.java
@@ -0,0 +1,51 @@
+package pl.altkom.asc.lab.micronaut.poc.payment.infrastructure.adapters.config;
+
+import io.micronaut.configuration.jdbc.hikari.DatasourceConfiguration;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.env.Environment;
+import io.micronaut.context.event.BeanCreatedEvent;
+import io.micronaut.context.event.BeanCreatedEventListener;
+
+import javax.inject.Singleton;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Singleton
+@Requires(notEnv = Environment.TEST)
+public class PostgresDatasourceConfigurationListener
+        implements BeanCreatedEventListener<DatasourceConfiguration> {
+
+    private final PostgresDatasourceProperties postgres;
+
+    public PostgresDatasourceConfigurationListener(PostgresDatasourceProperties postgres) {
+        this.postgres = postgres;
+    }
+
+    @Override
+    public DatasourceConfiguration onCreated(BeanCreatedEvent<DatasourceConfiguration> event) {
+        DatasourceConfiguration cfg = event.getBean();
+
+        if ("default".equals(cfg.getName())) {
+            cfg.setDriverClassName("org.postgresql.Driver");
+            cfg.setUsername(postgres.getUsername());
+            cfg.setPassword(postgres.getPassword());
+            cfg.setUrl(buildJdbcUrl());
+        }
+
+        return cfg;
+    }
+
+    private String buildJdbcUrl() {
+        String sslMode = postgres.isSsl() ? "require" : "disable";
+
+        return "jdbc:postgresql://" + postgres.getHost() + ":" + postgres.getPort()
+                + "/" + postgres.getDatabase()
+                + "?sslmode=" + sslMode
+                + "&user=" + encode(postgres.getUsername())
+                + "&password=" + encode(postgres.getPassword());
+    }
+
+    private String encode(String value) {
+        return URLEncoder.encode(value, StandardCharsets.UTF_8);
+    }
+}
