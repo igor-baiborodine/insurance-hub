aeecb1a0f87e55daa547a7d96f1cc278b0cd6164
Igor Baiborodine <igor.baiborodine@yandex.com>
2026-01-06 15:25:16 -0500

    feat: update script to build frontend with

--- legacy/build-frontend.sh
+++ legacy/build-frontend.sh
@@ -1,4 +1,4 @@
 yarn --cwd legacy/web-vue install
 [ $? -eq 0 ] || exit 1
-yarn --cwd legacy/web-vue run build
+VUE_APP_BACKEND_URL= VUE_APP_AUTH_URL= VUE_APP_CHAT_URL= yarn --cwd legacy/web-vue run build
 [ $? -eq 0 ] || exit 1
