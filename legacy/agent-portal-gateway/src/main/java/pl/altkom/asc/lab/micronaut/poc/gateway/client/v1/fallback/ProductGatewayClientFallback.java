14ffe4df8f12ef90c274f4f17da98e1620e4589a
Igor Baiborodine <igor.baiborodine@yandex.com>
2026-01-13 17:10:06 -0500

    fix issue with routing to product service

--- legacy/agent-portal-gateway/src/main/java/pl/altkom/asc/lab/micronaut/poc/gateway/client/v1/fallback/ProductGatewayClientFallback.java
+++ legacy/agent-portal-gateway/src/main/java/pl/altkom/asc/lab/micronaut/poc/gateway/client/v1/fallback/ProductGatewayClientFallback.java
@@ -3,6 +3,7 @@ package pl.altkom.asc.lab.micronaut.poc.gateway.client.v1.fallback;
 import io.micronaut.retry.annotation.Fallback;
 import io.reactivex.Maybe;
 import io.reactivex.Single;
+import lombok.extern.slf4j.Slf4j;
 import pl.altkom.asc.lab.micronaut.poc.gateway.client.v1.ProductGatewayClient;
 import pl.altkom.asc.lab.micronaut.poc.product.service.api.v1.ProductDto;
 
@@ -10,17 +11,20 @@ import javax.inject.Singleton;
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Singleton
 @Fallback
 public class ProductGatewayClientFallback implements ProductGatewayClient {
 
     @Override
     public Single<List<ProductDto>> getAll() {
+        log.warn("Fallback called for getAll(), empty result returned");
         return Single.just(Collections.emptyList());
     }
 
     @Override
     public Maybe<ProductDto> get(String productCode) {
+        log.warn("Fallback called for get(), empty result returned");
         return Maybe.empty();
     }
 }
